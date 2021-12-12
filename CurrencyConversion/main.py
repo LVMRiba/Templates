10c535289d16e323caee8792c9037359d404b5fa
Luiz Victor Moraes Riba <73717405+LVMRiba@users.noreply.github.com>
2021-12-11 22:49:33 -0300

    Update main.py

--- CurrencyConversion/main.py
+++ CurrencyConversion/main.py
@@ -1,8 +1,12 @@
 from CurrencyConversion import get_currency_quote
 
+
+# LER ARQUIVO DE PREÇOS
 with open("precos.csv", "r") as arquivo:
     lista_precos = arquivo.read().split("\n")
-
+    
+    
+# LOOP DE LEITURA DA INFORMAÇÃO DO ARQUIVO (linha à linha)
 for linha in lista_precos:
     mercadoria, valor, moeda = linha.split(",")
     cotacao = get_currency_quote(moeda, "BRL")
